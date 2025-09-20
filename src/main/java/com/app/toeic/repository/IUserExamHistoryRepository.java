96e030a9162389133ccbbe6ff34edb2523d18b88
OrionSDust <thienphong2071@gmail.com>
2025-09-20 21:31:00 +0700

    fix history exam

--- src/main/java/com/app/toeic/repository/IUserExamHistoryRepository.java
+++ src/main/java/com/app/toeic/repository/IUserExamHistoryRepository.java
@@ -1,5 +1,6 @@
 package com.app.toeic.repository;
 
+import com.app.toeic.model.UserAccount;
 import com.app.toeic.model.UserExamHistory;
 import com.app.toeic.response.UserExamHistoryVO;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,7 @@ import java.util.Optional;
 public interface IUserExamHistoryRepository extends JpaRepository<UserExamHistory, Integer> {
     @Query("SELECT u FROM UserExamHistory u JOIN FETCH u.exam WHERE u.userExamHistoryId = ?1")
     Optional<UserExamHistoryVO.UserExamHistoryGeneral> findUserExamHistoryByExamHistoryId(Integer userExamHistoryId);
+
+    @Query("SELECT u FROM UserExamHistory u JOIN FETCH u.exam WHERE u = ?1")
+    Optional<UserExamHistoryVO.UserExamHistoryGeneral> findUserExamHistoryByUser(UserAccount user);
 }
