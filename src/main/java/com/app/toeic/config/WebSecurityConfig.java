0ecbd701d8ad835055b78de49889d568c0e5d608
OrionSDust <thienphong2071@gmail.com>
2025-12-12 20:24:59 +0700

    feat: update CORS configuration to allow requests from the Vercel frontend URL

--- src/main/java/com/app/toeic/config/WebSecurityConfig.java
+++ src/main/java/com/app/toeic/config/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfiguration() {
         var corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(List.of("http://localhost:4200", "http://192.168.1.26:4200"));
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4200", "http://192.168.1.26:4200", "https://fe-kltn-35.vercel.app"));
         corsConfig.setAllowedMethods(List.of("*"));
         corsConfig.setAllowedHeaders(List.of("*"));
         corsConfig.addExposedHeader(Constant.CAPTCHA);
